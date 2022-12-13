e55876bb98252ff7846115273fff94389132a217
Catayo <619895339.com>
2022-12-13 18:24:09 +0800

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,15 @@
+public class HelloNumbers {
+	/**
+	 * prints out sum of numbers from 0 to 9.
+	 */
+	public static void main(String[] args) {
+		int sum = 0;
+		for (int i = 0; i < 9; i += 1) {
+			
+			sum += i;
+			System.out.println(sum);
+			
+		}
+
+	}
+}
